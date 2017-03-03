0cf7b199db3ca52746e6748b10ba84ebc116ea82
p-dr <ilidio@yandex.com>
2017-03-03 15:59:15 -0300

    Electron starts showing up.

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+const url = require('url');
+const path = require('path');
+
+
+// Express stuff ===============================================
+    
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
@@ -12,3 +18,20 @@ var server = app.listen(8081, function () {
 
     console.log('exemplo: ouvindo http://%s:%s', host, port)
 })
+
+// Electron stuff ==============================================
+
+const {app, BrowserWindow} = require('electron')
+const pug = require('electron-pug')
+
+app.on('ready', () => {
+
+    let win = new BrowserWindow()
+    win.loadUrl(url.format({
+        protocol: 'file';
+        pathname: path.join(__dirname, 'views', 'teacher-screen.pug')
+    }))
+
+})
+
+                
